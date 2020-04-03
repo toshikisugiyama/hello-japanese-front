653d6fe1256886aecc02bbb09529d0832e892520
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-04-03 22:34:55 +0900

    edit header navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,12 +22,10 @@ const Navbar = () => (
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item">
-              <figure className="image">
-                <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-              </figure>
+              <h1>Hello Japanese</h1>
             </Link>
           </div>
-          <div className="navbar-start">
+          <div className="navbar-end">
             {data.allWordpressPage.edges.map(edge => (
               <Link
                 className="navbar-item"
@@ -37,18 +35,6 @@ const Navbar = () => (
                 {edge.node.title}
               </Link>
             ))}
-          </div>
-          <div className="navbar-end">
-            <a
-              className="navbar-item"
-              href="https://github.com/GatsbyCentral/gatsby-starter-wordpress"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
           </div>
         </div>
       </nav>
